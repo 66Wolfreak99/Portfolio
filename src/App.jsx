2751bcc6c37a93d8093977708a46dd7b0f4c2cd0
Jeremy <jrmyargoud@gmail.com>
2025-05-04 10:37:21 +0200

    modifs

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import './App.scss'
 import List from './List'
 import Article from './Components/Article'
 
+import github from './assets/github.png'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,7 +16,7 @@ function App() {
      <div className='container'>
       <h1>Mon Portfolio</h1>
      <a href='https://github.com/66Wolfreak99' className="github">
-        <img src="/public/github.png" alt="Github" /> <span>Page Github</span>
+        <img src={github} alt="Github" /> <span>Page Github</span>
       </a>
       {List.map((item, index) => (
           <Article item={item} key={index}></Article>
