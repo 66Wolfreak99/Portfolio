512314c11a1afca8cea3bca88cdedc1729a48c9d
Jeremy <jrmyargoud@gmail.com>
2025-05-04 10:55:59 +0200

    ajout texte

--- src/List.js
+++ src/List.js
@@ -3,13 +3,13 @@ import site from './assets/site.png';
 import todo from './assets/todo.png';
 
 const List = [
-    {title: "Site web personnel", link: "https://66wolfreak99.github.io/index.html", github:"https://github.com/66Wolfreak99/66Wolfreak99.github.io", image: site, description: "Un site web personnel avec un style rétro début années 2000 qui montre des dessins fait à la tablette."},
+    {title: "Site web personnel", link: "https://66wolfreak99.github.io/index.html", github:"https://github.com/66Wolfreak99/66Wolfreak99.github.io", image: site, description: "Un site web personnel avec un style rétro 'début années 2000' qui montre des dessins fait à la tablette. Le site est composé de HTML et CSS purement vanilla avec du Javascript dans certaines pages pour y ajouter des fonctionnalités intéressantes telles que la page des personnages."},
 
     // {title: "Blablabook", link: "", github:"", image:"", description: "Un projet d'équipe créé pour notre formation créée avec React, Sass, Node Js et Express JS. C'est un site qui permet de rechercher des livres à l'aide de l'API OpenLbrary ainsi que de les ajouter dans sa bibliothèque personnelle. Dans une équipe de 3, j'ai été chargé de la majorité de la création du Front-end de l'application, ainsi qu'une partie du Back-end"},
 
-    {title: "Blue Dragon", link: "https://blue-dragon-ecru.vercel.app/", github:"https://github.com/66Wolfreak99/MonProjetPerso/tree/main/src/styles", image: blue_dragon, description: "Une simple page web purement statique qui représente un site de social media fictif. Créé avec du HTML basique et du Sass"},
+    {title: "Blue Dragon", link: "https://blue-dragon-ecru.vercel.app/", github:"https://github.com/66Wolfreak99/MonProjetPerso/tree/main/src/styles", image: blue_dragon, description: "Un projet libre pour ma formation. C'est une simple page web purement statique qui représente un site de social media fictif. Créé avec du HTML basique et du Sass. Puis, avec du javascript, la liste des posts est affichée avec la possibilité d'ajouter un post en texte seulement grâce un form. Comme c'est statique, rien n'est sauvegardé."},
 
-    {title: "Todo", link: "https://66wolfreak99.github.io/TodoApp/posts", github:"https://github.com/66Wolfreak99/MonProjetPerso/tree/main/src/styles", image: todo, description: "Ue petite application To-do créée avec angular. La liste est sauegardée dans le navigateur et il est possible de réorganiser l'ordre des éléments."}
+    {title: "Todo", link: "https://66wolfreak99.github.io/TodoApp/posts", github:"https://github.com/66Wolfreak99/MonProjetPerso/tree/main/src/styles", image: todo, description: "Ue petite application To-do créée avec angular. La liste est sauvegardée dans le navigateur fgrace au LocalStorage et il est possible de réorganiser l'ordre des éléments."}
 
 ]
 
